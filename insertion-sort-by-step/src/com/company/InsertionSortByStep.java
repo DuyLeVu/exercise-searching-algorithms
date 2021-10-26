98abd16088900f351275207309ee8de9a5397b06
DuyLeVu <levuduy132@gmail.com>
2021-10-26 09:39:53 +0700

    Sua bai insertion sort by step

--- insertion-sort-by-step/src/com/company/InsertionSortByStep.java
+++ insertion-sort-by-step/src/com/company/InsertionSortByStep.java
@@ -10,31 +10,33 @@ public class InsertionSortByStep {
         size = sc.nextInt();
         int[] numArr = new int[size];
         System.out.println("Nhập " + size + " phần tử");
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             numArr[i] = sc.nextInt();
             sc.nextLine();
         }
         System.out.println("Mảng vừa nhập: ");
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             System.out.print(numArr[i] + "\t");
         }
         System.out.println("\nThực hiện sắp xếp mảng ...");
         InsertionSortByStep(numArr);
     }
 
-    public static void InsertionSortByStep(int[] array){
-        for (int i = 1; i < array.length; i++){
+    public static void InsertionSortByStep(int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int temp = array[i];
             int pos = i;
-            while (pos > 0 && temp < array[pos-1]){
-                System.out.println("Đổi chỗ " + temp + " và " + array[pos-1]);
-                array[pos] = array[pos-1];
-                pos --;
+            while (pos > 0 && temp < array[pos - 1]) {
+                System.out.println("Đổi chỗ " + temp + " và " + array[pos - 1]);
+                array[pos] = array[pos - 1];
+                pos--;
             }
             array[pos] = temp;
-        }
-        for (int i = 0; i < array.length; i++){
-            System.out.print(array[i] + "\t");
+            System.out.println("Mảng sau khi sắp xếp lần thứ " + i);
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[j] + "\t");
+            }
+            System.out.println();
         }
     }
 }
