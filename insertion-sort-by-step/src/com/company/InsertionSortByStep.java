d9923cc287306d4a750edb061465df012977994c
DuyLeVu <levuduy132@gmail.com>
2021-10-26 09:33:43 +0700

    Add insertion-sort-by-step practice

---
+++ insertion-sort-by-step/src/com/company/InsertionSortByStep.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class InsertionSortByStep {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size;
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        size = sc.nextInt();
+        int[] numArr = new int[size];
+        System.out.println("Nhập " + size + " phần tử");
+        for (int i = 0; i < size; i++){
+            numArr[i] = sc.nextInt();
+            sc.nextLine();
+        }
+        System.out.println("Mảng vừa nhập: ");
+        for (int i = 0; i < size; i++){
+            System.out.print(numArr[i] + "\t");
+        }
+        System.out.println("\nThực hiện sắp xếp mảng ...");
+        InsertionSortByStep(numArr);
+    }
+
+    public static void InsertionSortByStep(int[] array){
+        for (int i = 1; i < array.length; i++){
+            int temp = array[i];
+            int pos = i;
+            while (pos > 0 && temp < array[pos-1]){
+                System.out.println("Đổi chỗ " + temp + " và " + array[pos-1]);
+                array[pos] = array[pos-1];
+                pos --;
+            }
+            array[pos] = temp;
+        }
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + "\t");
+        }
+    }
+}
